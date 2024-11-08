b98a4c33a3b9ee58971b2c0405b1a6171fd1cdc7
Juliana Silva <ju_snascimento@outlook.com>
2024-11-08 02:39:24 -0300

    add: section investiment

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const accordion = document.querySelector('#accordion');
+
+companies_investment.forEach(company =>
+    accordion.innerHTML += `
+        <div class="accordion-item mb-2">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${company.accordion}" aria-expanded="false" aria-controls="flush-collapse${company.accordion}">
+
+                <div class="col">
+                    <img src="${company.image}" alt="" class="img-fluid">
+                </div>
+                <div class="col">
+                    <h5>${company.name}</h5>
+                    <p class="btn btn-outline-dark bg-button">Tipo de energia: ${company.type}</p>
+                    <p>Localização: ${company.city}, ${company.state}</p>
+                    <p>Motivo para investimento: ${company.reason}</p>
+                    <p>Taxa de retorno: ${company.financial_return}</p>
+                    <a href="" class="btn btn-outline-dark bg-secundary text-light mt-2 text-center">Investir</a>
+                </div>
+              </button>
+            </h2>
+            <div id="flush-collapse${company.accordion}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="row p-3">
+                    <div class="col">
+                        <p>Valor do investimento: ${company.investment}</p>
+                        <p>Valor da cota: ${company.price}</p>
+                        <p>Valor atual arrecadado: ${company.value}</p>
+                    </div>
+                    <div class="col">
+                        <p>CNPJ: ${company.cnpj}</p>
+                        <p>Nome do responsável: ${company.responsible}</p>
+                        <p>Email: ${company.email} | Tel: ${company.tel}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+)
