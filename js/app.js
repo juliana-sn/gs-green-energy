5f35b7f74c7c3579276a2f2fc06ed93325ad6ec9
Juliana Silva <ju_snascimento@outlook.com>
2024-11-13 22:24:50 -0300

    add sections: reasons, services and objectives

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ const accordion = document.querySelector('#accordion');
 companies_investment.forEach(company =>
     accordion.innerHTML += `
         <div class="accordion-item mb-2">
-            <h2 class="accordion-header">
-              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${company.accordion}" aria-expanded="false" aria-controls="flush-collapse${company.accordion}">
-
-                <div class="col">
+            <h2 class="row accordion-header">
+              <button class="me-md-5 pe-md-5 accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${company.accordion}" aria-expanded="false" aria-controls="flush-collapse${company.accordion}">
+              <div class="row">
+                <div class="col-12 col-md-6">
                     <img src="${company.image}" alt="" class="img-fluid">
                 </div>
-                <div class="col">
+                <div class="col-12 col-md-6 mt-2 mt-md-0">
                     <h5>${company.name}</h5>
-                    <p class="btn btn-outline-dark bg-button">Tipo de energia: ${company.type}</p>
+                    <p>Tipo de energia: ${company.type}</p>
                     <p>Localização: ${company.city}, ${company.state}</p>
                     <p>Motivo para investimento: ${company.reason}</p>
                     <p>Taxa de retorno: ${company.financial_return}</p>
@@ -19,17 +19,16 @@ companies_investment.forEach(company =>
                     <a href="" class="btn btn-outline-dark bg-secundary text-light mt-2 text-center">Investir</a>
                 </div>
               </button>
-            </h2>
+              </div>
+              </h2>
             <div id="flush-collapse${company.accordion}" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                 <div class="row p-3">
                     <div class="col">
                         <p>Valor do investimento: ${company.investment}</p>
                         <p>Valor da cota: ${company.price}</p>
-                        <p>Valor atual arrecadado: ${company.value}</p>
                     </div>
                     <div class="col">
                         <p>CNPJ: ${company.cnpj}</p>
-                        <p>Nome do responsável: ${company.responsible}</p>
                         <p>Email: ${company.email} | Tel: ${company.tel}</p>
                     </div>
                 </div>
@@ -44,14 +43,22 @@ function renderCards(companies) {
     cardsContainer.innerHTML = '';
     companies.forEach(company => {
         cardsContainer.innerHTML += `
-            <div class="card col card-hire m-2" style="width: 18rem;" data-city="${company.city}">
-                <img src="..." class="card-img-top" alt="...">
+          
+        
+          <div class=" col col-md-6 col-lg-3">
+            <div class="card mx-auto card-hire m-2"   data-city="${company.city}" style="width: 13rem;"> 
+                <img src="${company.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${company.name}</h5>
-                    <p class="card-text">${company.city}</p>
-                    <a href="#" class="btn btn-primary">Contratar</a>
+                    <p>${company.type}</p>
+                    <p class="card-text">${company.city} | ${company.state}</p>
+                    <a href="#" class="btn btn-primary text-center">Consultar planos</a>
                 </div>
+                
+                
             </div>
+          </div>
+        
         `;
     });
 }
@@ -65,7 +72,7 @@ function filterCards() {
     );
 
     if (filteredCompanies.length === 0) {
-        document.querySelector('#companies-hire').innerHTML = '<p class="text-center">No momento não temos disponibilidade para essa cidade. Mas estamos trabalhando para expandir o nosso negócio o quanto antes!</p>';
+        document.querySelector('#companies-hire').innerHTML = '<p class="text-center">No momento não temos disponibilidade para essa cidade, mas estamos trabalhando para expandir o nosso negócio o quanto antes!</p>';
     } else {
         renderCards(filteredCompanies);
     }
--- js/data.js
+++ js/data.js
@@ -4,99 +4,80 @@ const companies_investment = [{
     name: 'Solar',
     city: 'São Paulo',
     state: 'SP',
-    reason: 'Expansão',
+    reason: 'Se manter no mercado',
     type: 'Solar',
-    investment: 'R$10000',
-    value: 'R$2000',
+    investment: 'R$1.000.000',
     price: 'R$100',
-    financial_return: '11%',
+    financial_return: '11% ao ano',
     date_return: '05/06/2025',
     cnpj: '000000',
     email: 'exemplo@email.com',
     tel: '(99)99999-9999',
-    responsible: 'Fulano',
     image: '../images/solar-energy.png'
 },
 {
     id: 2,
     accordion: 'Two', 
     name: 'Eólica Offshore',
-    city: 'São Paulo',
-    state: 'SP',
+    city: 'Rio de Janeiro',
+    state: 'RJ',
     reason: 'Criação',
     type: 'Eólica Offshore',
-    investment: 'R$10000',
-    value: 'R$2000',
-    price: 'R$100',
-    financial_return: '11%',
-    date_return: '05/06/2025',
+    investment: 'R$20.000.000',
+    price: 'R$400',
+    financial_return: '15% ao ano',
+    date_return: '05/06/2027',
     cnpj: '000000',
     email: 'exemplo@email.com',
     tel: '(99)99999-9999',
-    responsible: 'Fulano',
     image: '../images/wind-offshore.png'
 },
 {
     id: 3,
     accordion: 'Three', 
     name: 'Eólica',
-    city: 'São Paulo',
-    state: 'SP',
-    reason: 'Se manter no mercado',
+    city: 'Parnaíba',
+    state: 'PI',
+    reason: 'Expansão',
     type: 'Eólica',
-    investment: 'R$10000',
-    value: 'R$2000',
-    price: 'R$100',
-    financial_return: '11%',
-    date_return: '05/06/2025',
+    investment: 'R$10.000.000',
+    price: 'R$300',
+    financial_return: '12% ao ano',
+    date_return: '08/12/2026',
     cnpj: '000000',
     email: 'exemplo@email.com',
     tel: '(99)99999-9999',
-    responsible: 'Fulano',
     image: '../images/wind.png'
 }
 ]
-
 
 const companies_hire = [
 {
     name: 'Empresa 1',
     city: 'São Paulo',
     state: 'SP',
-    type: 'Eólica',
-    email: 'exemplo@email.com',
-    tel: '(99)99999-9999',
-    responsible: 'Fulano',
-    image: ''
+    type: 'Solar',
+    image: '../images/solar-energy.png'
 },
 {
     name: 'Empresa 2',
     city: 'Rio de Janeiro',
     state: 'RJ',
     type: 'Eólica',
-    email: 'exemplo@email.com',
-    tel: '(99)99999-9999',
-    responsible: 'Fulano',
-    image: ''
+    image: '../images/wind.png'
 },
 {
     name: 'Empresa 3',
-    city: 'São Paulo',
-    state: 'SP',
-    type: 'Eólica',
-    email: 'exemplo@email.com',
-    tel: '(99)99999-9999',
-    responsible: 'Fulano',
-    image: ''
+    city: 'Fortaleza',
+    state: 'CE',
+    type: 'Eólica Offshore',
+    image: '../images/wind-offshore.png'
 },
 {
     name: 'Empresa 4',
-    city: 'Suzano',
-    state: 'SP',
-    type: 'Eólica',
-    email: 'exemplo@email.com',
-    tel: '(99)99999-9999',
-    responsible: 'Fulano',
-    image: ''
+    city: 'Brasília',
+    state: 'DF',
+    type: 'Solar',
+    image: '../images/solar-energy.png'
 }
 ]
